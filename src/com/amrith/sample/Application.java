890e7c9f39aaf826c0fa14129442884323c6801a
amrithhun <amm.amrith@gmail.com>
2020-02-29 15:47:31 +0530

    li

--- src/com/amrith/sample/Application.java
+++ src/com/amrith/sample/Application.java
@@ -1,5 +1,7 @@
 package com.amrith.sample;
 
+import sun.management.Sensor;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -8,7 +10,7 @@ public class Application {
     public static void main(String[] args) {
 
 
-        List<String> student=new ArrayList<>();
+  /*      List<String> student=new ArrayList<>();
         student.add("Amrith");
         student.add("Aqueel");
         student.add("Falil");
@@ -19,6 +21,26 @@ public class Application {
                 .filter(s->s.length()>5)
                 .collect(Collectors.toList());
         System.out.println(filtereddata);
+*/
+/*  Sensors sensor=new Sensors();
+  sensor.setName("main door");
+  sensor.setValue((double) 25);
+*/
+List<Sensors> sensors=new ArrayList<>();
+Sensors sensor1=new Sensors("Living Room",28);
+Sensors.add(sensor1);
+Sensors sensor2 = new Sensors("Office Room",26);
+Sensors.add(sensor2);
+Sensors sensor3 = new Sensors("Bed Room",31);
+Sensors.add(sensor3);
+Sensors sensor4 = new Sensors("Bath Room",25);
+Sensors.add(sensor4);
+
+
+
+
+
+
 
     }
 }
--- src/com/amrith/sample/Sensors.java
+++ src/com/amrith/sample/Sensors.java
@@ -1,4 +1,37 @@
 package com.amrith.sample;
 
 public class Sensors {
+    private String name;
+    private Double value;
+
+    public Sensors() {
+    }
+
+    public Sensors(String name, Double value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public Sensors(String living_room, int i) {
+    }
+
+    public static void add(Sensors sensor2) {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
 }
