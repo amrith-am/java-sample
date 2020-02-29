77eada6b736d01143a0f8eeed296ef8edd85210b
amrithhun <amm.amrith@gmail.com>
2020-02-29 14:42:18 +0530

    Aplication

---
+++ src/com/amrith/sample/Application.java
@@ -0,0 +1,10 @@
+package com.amrith.sample;
+
+public class Application {
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello JAVA");
+        System.out.println();
+    }
+}
