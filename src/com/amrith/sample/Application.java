9b76dc644a9842631285d404c7a958c64b8adb6d
amrithhun <amm.amrith@gmail.com>
2020-02-29 14:57:17 +0530

    updated

--- src/com/amrith/sample/Application.java
+++ src/com/amrith/sample/Application.java
@@ -1,10 +1,24 @@
 package com.amrith.sample;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Application {
     public static void main(String[] args) {
 
 
-        System.out.println("Hello JAVA");
-        System.out.println();
+        List<String> student=new ArrayList<>();
+        student.add("Amrith");
+        student.add("Aqueel");
+        student.add("Falil");
+        student.add("FAris");
+        student.add("Yusuf");
+        System.out.println(student);
+        List<String> filtereddata=student.stream()
+                .filter(s->s.length()>5)
+                .collect(Collectors.toList());
+        System.out.println(filtereddata);
+
     }
 }
